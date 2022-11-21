f1e079f4b29ec98d1a098fab49e38b12fb3c0630
Aminul <mdaminulislamsahin14@gmail.com>
2022-11-21 22:38:33 +0600

    booking id api

--- index.js
+++ index.js
@@ -81,6 +81,13 @@ async function run() {
             res.send(result)
         });
 
+        app.get('/bookings/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id : ObjectId(id)}
+            const result = await bookingsCollection.findOne(query);
+            res.send(result)
+        })
+
         app.get('/bookings', verifyJwt, async(req, res) =>{
             const email = req.query.email;
             const decodedEmail = req.decoded.email
